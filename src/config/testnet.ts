94152d63f87265731a781d674732638bd428fb70
ufogreys <45027262+ufogreys@users.noreply.github.com>
2023-06-09 04:00:49 -0300

    Update testnet.ts

--- src/config/testnet.ts
+++ src/config/testnet.ts
@@ -4,7 +4,6 @@ import {Token} from '@layerzerolabs/x-trader-joe-bridge';
 const tokens = [
   new Token(ChainId.FUJI, '0xcc14EcF814aAC6015bb38bc87296F28f8470eE13', 18, 'JOE'),
   new Token(ChainId.BSC_TESTNET, '0x845C442d3B6B5cCC32Fd0192b196b8e155Db508B', 18, 'JOE'),
-  new Token(ChainId.ARBITRUM_GOERLI, '0x0cCd82C5E94F93913b3227B8069F598E2708d442', 18, 'JOE'),
 ];
 
 const proxy = [
